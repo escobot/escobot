9a4397d9c2ea5eabb44b4486a965cb6946100e09
pbgnz <pabgonza1995@gmail.com>
2017-06-07 22:27:51 -0400

    set natural language processing

--- bin/run.js
+++ bin/run.js
@@ -1,16 +1,24 @@
 'use strict';
 
-const slackClient = require('../server/slackClient');
+// api keys
+const config = require('../config');
+const slackToken = config.slackToken;
+const witToken = config.witToken;
 
+// clients
+const slackClient = require('../server/slackClient');
+const witClients = require('../server/witClient')(witToken);
+
+// logs
+const slackLogLevel = 'verbose';
+
+// server
 const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
-const config = require('../config');
-const slackToken = config.slackToken;
-
-const slackLogLevel = 'verbose';
-const rtm = slackClient.init(slackToken, slackLogLevel);
+// real time messages
+const rtm = slackClient.init(slackToken, slackLogLevel, witClients);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
