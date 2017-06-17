6923799aae0c3f40d3421fe91e2dd36eadccb309
pbgnz <pabgonza1995@gmail.com>
2017-06-17 12:38:21 -0400

    add endpoint security

--- tellme/config/index.js
+++ tellme/config/index.js
@@ -18,6 +18,7 @@ module.exports = {
     witToken: process.env.WIT_TOKEN,
     slackLogLevel: 'verbose',
     serviceTimeout: 30,
+    tellmeApiToken: process.env.TELLME_API_TOKEN,
     log: (env) => {
         if(env) return log[env]();
         return log[process.env.NODE_ENV || 'development']();
--- tellme/test/server/service.test.js
+++ tellme/test/server/service.test.js
@@ -9,8 +9,8 @@ describe('The express service', () => {
     describe('PUT /foo', () => {
         it('should return HTTP 404', (done) => {
             request(service)
-            .put('/foo')
-            .expect(404, done);
+                .put('/foo')
+                .expect(404, done);
         });
     });
 });
@@ -18,12 +18,29 @@ describe('The express service', () => {
 describe('PUT /service/:intent/:port', () => {
     it('should return HTTP 200 with valid result', (done) => {
         request(service)
-        .put('/service/test/9999')
-        .expect(200)
-        .end((err, res) => {
-            if(err) return done(err);
-            res.body.result.should.startWith('test at [::ffff:127.0.0.1]:9999');
-            return done();
-        });
+            .put('/service/test/9999')
+            .set('X-TELLME-API-TOKEN', config.tellmeApiToken)
+            .set('X-TELLME-SERVICE-TOKEN', 'HEYE')
+            .expect(200)
+            .end((err, res) => {
+                if(err) return done(err);
+                res.body.result.should.startWith('test at [::ffff:127.0.0.1]:9999');
+                return done();
+            });
+    });
+
+    it('should return HTTP 403 with no api token provided', (done) => {
+        request(service)
+            .put('/service/test/9999')
+            .expect(403)
+            .end(done);
+    });
+
+    it('should return HTTP 400 with no service token provided', (done) => {
+        request(service)
+            .put('/service/test/9999')
+            .set('X-TELLME-API-TOKEN', config.tellmeApiToken)
+            .expect(400)
+            .end(done);
     });
 });
